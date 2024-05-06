0f087c94272d4294649464bd93daf5fccd2de360
saurabhnaphade <naphadesaurabh@gmail.com>
2024-05-06 21:04:04 +0530

    Kafka Producer Template added

---
+++ Wikimedia-Producer/src/test/java/com/saurabh/kafka/WikimediaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
