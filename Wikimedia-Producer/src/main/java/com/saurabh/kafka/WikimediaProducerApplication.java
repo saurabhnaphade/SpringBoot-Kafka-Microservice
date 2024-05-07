027a25471c21bceeb6ebddb64dfd449ddcce4972
saurabhnaphade <naphadesaurabh@gmail.com>
2024-05-07 08:14:31 +0530

    kafka producer implemented

--- Wikimedia-Producer/src/main/java/com/saurabh/kafka/WikimediaProducerApplication.java
+++ Wikimedia-Producer/src/main/java/com/saurabh/kafka/WikimediaProducerApplication.java
@@ -1,13 +1,22 @@
 package com.saurabh.kafka;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WikimediaProducerApplication {
+public class WikimediaProducerApplication implements CommandLineRunner {
+
+	@Autowired
+	private WikimediaChangesProducer wikimediaChangesProducer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WikimediaProducerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		wikimediaChangesProducer.sendMessage();
+	}
 }
