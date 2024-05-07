dd38380a4dae7c04b990ebac7285d573653a9aff
saurabhnaphade <naphadesaurabh@gmail.com>
2024-05-07 20:00:51 +0530

    kafka consumer implemented

---
+++ Wikimedia-Kafka-Consumer/src/test/java/com/saurabh/WikimediaKafkaConsumer/WikimediaKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.WikimediaKafkaConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
